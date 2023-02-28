f0e012e20303db0890f130db619a6842f75545b0
Selhattin ENÇ <92638910+selhatinenc@users.noreply.github.com>
2023-02-28 10:34:13 +0300

    Delete book_register_system directory

--- book_register_system/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.sevgiepozhan191030043 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.sevgiepozhan191030043 to javafx.fxml;
-    exports com.example.sevgiepozhan191030043;
-}
